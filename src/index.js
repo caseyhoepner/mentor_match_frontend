af988186d049b6dde0606202dd32b87ab5523797
Casey Hoepner <caseyhoepner@gmail.com>
2018-12-18 14:55:58 -0700

    Add reducers, actions, thunks folders and imported appropriate things into to app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers'
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+ReactDOM.render(
+<Provider store={store}>
+  <BrowserRouter >
+    <App />
+  </BrowserRouter>
+</Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
