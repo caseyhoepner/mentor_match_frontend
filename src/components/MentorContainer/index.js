5366a389e54f3946d3798e2399ee049622254fbc
Alexander Rau <alexanderrau86@yahoo.com>
2018-12-19 15:03:14 -0700

    Build out and style all basic elements on the MentorContainer header area

--- src/components/MentorContainer/index.js
+++ src/components/MentorContainer/index.js
@@ -10,7 +10,18 @@ class MentorContainer extends Component {
     return (
       <div>
         <header className='mc-header'>
-          <img src={require('../../utils/assets/turing-logo.png')} alt='Turing Logo' />
+          <div className='mc-nav-btns'>
+            <img src={require('../../utils/assets/turing-logo.png')} alt='Turing Logo' className='mc-turing-logo' />
+            <div className='mc-github-btn'>
+              <p className='mc-gh-tagline'>Sign in with GitHub</p>
+              <img src={require('../../utils/assets/github-logo.svg')} alt='GitHub Logo' className='mc-github-logo' />
+            </div>
+          </div>
+          <div className='mc-title'>
+            <h1>Mentor Profiles</h1>
+            <div className='mc-line-break'></div>
+            <p className='mc-tagline'>Browse current mentors to find your match!</p>
+          </div>
         </header>
       </div>
       )
