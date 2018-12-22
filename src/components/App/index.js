11592fd1b4675be9b2e7410b7b61efaa80e6f9d3
Casey Hoepner <caseyhoepner@gmail.com>
2018-12-21 18:28:11 -0700

    Build NewMentorForm

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Login from '../../containers/Login'
+import AdminDashboard from '../../containers/AdminDashboard';
 import MentorContainer from '../MentorContainer'
+import NewMentorForm from '../../containers/NewMentorForm'
 import { Route, withRouter, Switch } from 'react-router-dom';
 import './App.css';
-
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Route path='/login' component={Login} /> */}
+        <Route path='/login' component={Login} />
         <Route exact path='/' component={MentorContainer} />
+        <Route exact path='/new-mentor-form' component={NewMentorForm} />
         {/* <Route path='/edit-mentor' component={MentorCard} /> */}
         {/* <Route path='/mentor-profile' component={MentorProfile} /> */}
-        {/* <Route path='/admin-dashboard' component={AdminDashboard} /> */}
+        <Route path='/admin-dashboard' component={AdminDashboard} />
       </div>
     );
   }
--- src/containers/AdminDashboard/index.js
+++ src/containers/AdminDashboard/index.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import './AdminDashboard.css';
+import AdminMentorSearch from '../../containers/AdminMentorSearch';
 
-export class AdminDashboard extends Component {
+class AdminDashboard extends Component {
   constructor() {
-    super()
+    super();
   }
 
   render() {
     return (
-      <div>AdminDashboard</div>
-      )
+      <div>
+        <header className='ad-header'>
+          <div className='ad-nav-btns'>
+            <img src={require('../../utils/assets/turing-logo.png')} alt='Turing Logo' className='ad-turing-logo' />
+            <div className='ad-github-btn'>
+              <p className='ad-gh-tagline'>Sign in with GitHub</p>
+              <img src={require('../../utils/assets/github-logo.svg')} alt='GitHub Logo' className='ad-github-logo' />
+            </div>
+          </div>
+          <div className='ad-title'>
+            <h1 className='ad-title-text'>Admin Dashboard</h1>
+            <div className='ad-line-break'></div>
+            <p className='ad-tagline'>Filter mentors and students to find a match!</p>
+          </div>
+        </header>
+
+        <AdminMentorSearch />
+
+      </div>
+    )
   }
 }
 
