2fd84900d6a22c3429be5348c581b35c261f273e
Alexander Rau <alexanderrau86@yahoo.com>
2018-12-27 17:44:53 -0700

    Fully test reducers

--- src/reducers/reducers.test.js
+++ src/reducers/reducers.test.js
@@ -1,6 +1,6 @@
 import * as SearchActions from '../actions/search-actions';
-import { setLocale, setSearch } from './search-reducers';
-import { setMentors, isLoading, hasErrored } from './mentors-reducers';
+import { setLocale, setSearch, toggleShowingMentors } from './search-reducers';
+import { setMentors, setMentorModal, isLoading, hasErrored } from './mentors-reducers';
 
 describe('isLoading', () => {
   it('should update state with the isLoading boolean', () => {
@@ -11,6 +11,16 @@ describe('isLoading', () => {
     const expected = true
 
     const result = isLoading(false, mockAction)
+    expect(result).toEqual(expected)
+  });
+
+  it('should return default state if nothing is passed in', () => {
+    const mockAction = {
+      isLoading: true
+    }
+    const expected = false
+
+    const result = isLoading(undefined, mockAction)
     expect(result).toEqual(expected)
   });
 });
@@ -24,6 +34,16 @@ describe('hasErrored', () => {
     const expected = true
 
     const result = hasErrored(false, mockAction)
+    expect(result).toEqual(expected)
+  });
+
+  it('should return default state if nothing is passed in', () => {
+    const mockAction = {
+      hasErrored: true
+    }
+    const expected = false
+
+    const result = hasErrored(undefined, mockAction)
     expect(result).toEqual(expected)
   });
 });
@@ -43,7 +63,7 @@ describe('setLocale reducer', () => {
   it('should return original state as default', () => {
     const mockLocale = 'Remote'
 
-    const result = setLocale('', mockLocale)
+    const result = setLocale(undefined, mockLocale)
     expect(result).toEqual('')
   });
 });
@@ -63,8 +83,29 @@ describe('setSearch', () => {
   it('should return original state as default', () => {
     const mockSearchTerm = 'skwijb'
 
-    const result = setSearch('', mockSearchTerm)
+    const result = setSearch(undefined, mockSearchTerm)
     expect(result).toEqual('')
+  });
+});
+
+describe('toggleShowingMentors', () => {
+  it('should toggle the state if all mentors are showing', () => {
+    const mockAction = {
+      type: "TOGGLE_SHOWING_MENTORS",
+      showingAllMentors: true
+    }
+
+    const result = toggleShowingMentors(undefined, mockAction)
+    expect(result).toEqual(true)
+  });
+
+  it('should return false as the default state', () => {
+    const mockAction = {
+      showingAllMentors: true
+    }
+
+    const result = toggleShowingMentors(undefined, mockAction)
+    expect(result).toEqual(false)
   });
 });
 
@@ -78,5 +119,55 @@ describe('setMentors', () => {
 
     const result = setMentors([], mockAction)
     expect(result).toEqual(expected)
+  });
+
+  it('should update existing mentor array in state with changes', () => {
+    const mockState = [ 
+      { id: 2, name: 'Stannis', preferences: { title: 'Doin\' stuff' } },
+      { id: 3, name: 'Maurey', preferences: { title: 'Doin\' more stuff' } }
+    ]
+    const mockAction = {
+      type: "UPDATE_CHANGED_MENTOR",
+      mentor: { id: 2, name: 'Robert', preferences: { title: 'Doin\' stuff' } }
+    }
+    const expected = [ 
+      { id: 2, name: 'Robert', preferences: { title: 'Doin\' stuff' } },
+      { id: 3, name: 'Maurey', preferences: { title: 'Doin\' more stuff' } }
+    ]
+
+    const result = setMentors(mockState, mockAction)
+    expect(result).toEqual(expected)
+  });
+
+  it('should return original state as default', () => {
+    const mockAction = {
+      mentors: [ { name: 'Stannis', preferences: { title: 'Doin\' stuff' } } ]
+    }
+    const expected = []
+
+    const result = setMentors(undefined, mockAction)
+    expect(result).toEqual(expected)
+  });
+});
+
+describe('setMentorModal', () => {
+  it('should update state with new mentor object for the modal', () => {
+    const mockAction = {
+      type: "SET_MENTOR_MODAL",
+      modalInfo: { name: 'carrie hairy', city: 'halle berry', preferences: { title: 'Doin\' Stuff' } }
+    }
+    const expected = { name: 'carrie hairy', city: 'halle berry', preferences: { title: 'Doin\' Stuff' } }
+
+    const result = setMentorModal({}, mockAction)
+    expect(result).toEqual(expected)
+  });
+
+  it('should return state of null as a default', () => {
+    const mockAction = {
+      modalInfo: { name: 'carrie hairy', city: 'halle berry', preferences: { title: 'Doin\' Stuff' } }
+    }
+    
+    const result = setMentorModal(undefined, mockAction)
+    expect(result).toEqual(null)
   });
 });
