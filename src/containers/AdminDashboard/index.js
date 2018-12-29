e9de11a81a58231e827c31fc478f0b6f1c4b2a79
Casey Hoepner <caseyhoepner@gmail.com>
2018-12-29 09:53:10 -0600

    Refactor AdminDashboard

--- src/containers/AdminDashboard/index.js
+++ src/containers/AdminDashboard/index.js
@@ -17,6 +17,11 @@ export class AdminDashboard extends Component {
 
   render() {
     let mentorCards;
+    let modal;
+
+    if (this.props.modalInfo) {
+      modal = <AdminMentorModal currentMentor={this.props.modalInfo} />
+    }
 
     if (!this.props.showingAllMentors) {
       const filteredMentors = this.props.mentors.filter(mentor => {
@@ -31,7 +36,6 @@ export class AdminDashboard extends Component {
       })
     }
 
-    if (this.props.modalInfo) {
       return (
         <div>
           <header className='ad-header'>
@@ -48,35 +52,7 @@ export class AdminDashboard extends Component {
               <p className='ad-tagline'>Filter mentors and students to find a match!</p>
             </div>
           </header>
-          <AdminMentorModal currentMentor={this.props.modalInfo} />
-          <AdminMentorSearch />
-          <div className="ad-mentor-headings">
-            <h1 className='ad-mentor-heading'>Profile Picture</h1>
-            <h1 className='ad-mentor-heading'>Name</h1>
-            <h1 className='ad-mentor-heading'>Stack Preference</h1>
-            <h1 className='ad-mentor-heading'>Other Preferences</h1>
-            <h1 className='ad-mentor-heading'>View/Edit</h1>
-          </div>
-          { mentorCards }
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          <header className='ad-header'>
-            <div className='ad-nav-btns'>
-              <img src={require('../../utils/assets/turing-logo.png')} alt='Turing Logo' className='ad-turing-logo' />
-              <div className='ad-github-btn'>
-                <p className='ad-gh-tagline'>Sign in with GitHub</p>
-                <img src={require('../../utils/assets/github-logo.svg')} alt='GitHub Logo' className='ad-github-logo' />
-              </div>
-            </div>
-            <div className='ad-title'>
-              <h1 className='ad-title-text'>Admin Dashboard</h1>
-              <div className='ad-line-break'></div>
-              <p className='ad-tagline'>Filter mentors and students to find a match!</p>
-            </div>
-          </header>
+          { modal }
           <AdminMentorSearch />
           <div className="ad-mentor-headings">
             <h1 className='ad-mentor-heading'>Profile Picture</h1>
@@ -90,7 +66,6 @@ export class AdminDashboard extends Component {
       )
     }
   }
-}
 
 export const mapStateToProps = (state) => ({
   mentors: state.mentors,
